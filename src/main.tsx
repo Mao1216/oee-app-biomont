63c7de21b852a0407e214d9d34e0a1277c188ede
Mao1216 <maolin273@gmail.com>
2026-08-18 14:57:16 -0500

    chore: add src/main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import OEEApplication from './OEEApplication';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <OEEApplication />
+  </React.StrictMode>,
+);
